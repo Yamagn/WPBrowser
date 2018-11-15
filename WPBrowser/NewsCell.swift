ec176407bffe96218c37ba6fc131c97f411aea46
Yamagn <syanekenpon@gmail.com>
2018-11-15 23:33:21 +0900

    initial commit

---
+++ WPBrowser/NewsCell.swift
@@ -0,0 +1,24 @@
+//
+//  NewsCell.swift
+//  WPBrowser
+//
+//  Created by ymgn on 2018/11/15.
+//  Copyright © 2018 ymgn. All rights reserved.
+//
+
+import UIKit
+
+class NewsCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ WPBrowser/NewsViewController.swift
@@ -0,0 +1,32 @@
+//
+//  NewsViewController.swift
+//  WPBrowser
+//
+//  Created by ymgn on 2018/11/15.
+//  Copyright © 2018 ymgn. All rights reserved.
+//
+
+import UIKit
+
+class NewsViewController: UIViewController, UITableViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
