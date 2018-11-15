66e1c82ae1b3bee3ac1756a7971ee7990751a81d
Yamagn <syanekenpon@gmail.com>
2018-11-15 23:02:24 +0900

    Initial Commit

---
+++ WPBrowser/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WPBrowser
+//
+//  Created by ymgn on 2018/11/15.
+//  Copyright © 2018 ymgn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ WPBrowserTests/WPBrowserTests.swift
@@ -0,0 +1,34 @@
+//
+//  WPBrowserTests.swift
+//  WPBrowserTests
+//
+//  Created by ymgn on 2018/11/15.
+//  Copyright © 2018 ymgn. All rights reserved.
+//
+
+import XCTest
+@testable import WPBrowser
+
+class WPBrowserTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
